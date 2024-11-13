7fd80628a0d04cb316dbec84fd39c85e53bc8a8d
Lucas <lucas.tello@pinapp.tech>
2024-11-13 19:54:33 -0300

    Ajusto form para dispositivos mas pequenos

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
 
   return (
-    <footer className='mt-auto text-center pb-1 bg-[#010101]'>
+    <footer className='mt-auto text-center py-2 bg-[#010101]'>
         <p className='text-xs text-[#f2f2f2]'>Developed by <a href='https://www.linkedin.com/in/tellolucas/' className='font-bold'>@lxcste_</a></p>
     </footer>
   )
